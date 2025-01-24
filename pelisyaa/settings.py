50970b7d268f468ce9cacea63160084698086a1b
Adrian4891 <ernebarra36@gmail.com>
2025-01-24 12:16:00 -0300

    title page

--- pelisyaa/settings.py
+++ pelisyaa/settings.py
@@ -78,16 +78,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pelisyaa.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.config(
